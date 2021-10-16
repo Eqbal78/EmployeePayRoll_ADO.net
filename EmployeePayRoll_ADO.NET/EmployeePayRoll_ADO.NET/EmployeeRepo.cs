6e0980631a1521ec3ce11ddfe5d34e45fc73d044
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-10-16 12:33:53 +0530

    Create Payroll Service Database Connection

---
+++ EmployeePayRoll_ADO.NET/EmployeePayRoll_ADO.NET/EmployeeRepo.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EmployeePayRoll_ADO.NET
+{
+    public class EmployeeRepo
+    {
+        /// <summary>
+        /// Database server store in string
+        /// </summary>
+        public static string connectionString = @"Data Source=DESKTOP-AB32E02;Initial Catalog=payroll_service;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        /// <summary>
+        /// check database connection methods
+        /// </summary>
+        public void DBconnection()
+        {
+            //open server
+            connection.Open();
+            if (connection.State == ConnectionState.Open)
+                Console.WriteLine("Database connection is succesfull & is opened !!");
+            else
+                Console.WriteLine("Connection is not established !!");
+            connection.Close();
+        }
+
+        /// <summary>
+        /// Fetch all data method
+        /// </summary>
+        public void GetAllEmployees()
+        {
+            //instance of EmployeeModel class
+            EmployeeModel model = new EmployeeModel();
+            try
+            {
+                using (connection)
+                {
+                    string query = @"select * from employee_payroll";
+                    //define the SqlCommand object
+                    SqlCommand command = new SqlCommand(query, connection);
+                    connection.Open();
+                    //define SqlDataReader Object
+                    SqlDataReader reader = command.ExecuteReader();
+                    //check if there are record
+                    if (reader.HasRows)
+                    {
+                        Console.Write("EmpID" + "\t" + "Name" + "\t\t" + "Gender" + "\t");
+                        Console.Write("Department" + "\t" + "PhoneNumber" + "\t" + "Address" + "\t\t" + "StartDate" + "\n");
+                        while (reader.Read())
+                        {
+                            model.EmployeeID = reader.GetInt32(0);
+                            model.EmployeeName = reader["name"].ToString();
+                            model.PhoneNumber = reader["phone"].ToString();
+                            model.Address = reader["address"].ToString();
+                            model.Department = reader["department"].ToString();
+                            model.Gender = Convert.ToChar(reader["Gender"].ToString().Trim());
+                            model.BasicPay = Convert.ToInt32(reader["basic_pay"]);
+                            model.Deductions = Convert.ToDouble(reader["deduction"]);
+                            model.TaxablePay = Convert.ToDouble(reader["taxable_pay"]);
+                            model.IncomeTax = Convert.ToDouble(reader["incomeTax"]);
+                            model.NetPay = Convert.ToDouble(reader["net_pay"]);
+                            model.StartDate = Convert.ToDateTime(reader["start_date"]);
+                            
+                            Console.Write("{0}\t{1}\t\t{2}\t", model.EmployeeID, model.EmployeeName,model.Gender);
+                            Console.Write("{0}\t\t{1}\t{2}\t{3}\n", model.Department, model.PhoneNumber, model.Address, model.StartDate.ToString("dd-mm-yyyy"));
+                        }
+                    }
+                    else
+                        Console.WriteLine("No data found");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ EmployeePayRoll_ADO.NET/EmployeePayRoll_ADO.NET/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmployeePayRoll_ADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee PayRoll Service.");
+            EmployeeRepo employeeRepo = new EmployeeRepo();
+            employeeRepo.Method1();
+            employeeRepo.GetAllEmployees();
+        }
+    }
+}
