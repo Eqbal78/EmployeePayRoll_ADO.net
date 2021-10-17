0a91a1683e801006864b59cb8eb1c3ba380c5692
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-10-17 11:22:08 +0530

    Ability to retrieve Employee payroll With Date Range from database added

--- EmployeePayRoll_ADO.NET/EmployeePayRoll_ADO.NET/EmployeeRepo.cs
+++ EmployeePayRoll_ADO.NET/EmployeePayRoll_ADO.NET/EmployeeRepo.cs
@@ -163,5 +163,60 @@ namespace EmployeePayRoll_ADO.NET
             }
         }
 
+
+        /// <summary>
+        /// Employee details with date range
+        /// </summary>
+        public void GetAllEmployeesFromDate()
+        {
+            //instance of EmployeeModel class
+            EmployeeModel model = new EmployeeModel();
+            try
+            {
+                using (connection)
+                {
+                    string query = @"select * from employee_payroll where start_date between cast('2020-01-01' as date) and cast(getdate() as date)";
+                    //define the SqlCommand object
+                    SqlCommand command = new SqlCommand(query, connection);
+                    connection.Open();
+                    //define SqlDataReader Object
+                    SqlDataReader reader = command.ExecuteReader();
+                    //check if there are record
+                    if (reader.HasRows)
+                    {
+                        Console.Write("EmpID" + "\t" + "Name" + "\t\t" + "Gender" + "\t");
+                        Console.Write("Department" + "\t" + "PhoneNumber" + "\t" + "Address" + "\t\t" + "StartDate" + "\n");
+                        while (reader.Read())
+                        {
+                            model.EmployeeID = reader.GetInt32(0);
+                            model.EmployeeName = reader["name"].ToString();
+                            model.PhoneNumber = reader["phone"].ToString();
+                            model.Address = reader["address"].ToString();
+                            model.Department = reader["department"].ToString();
+                            model.Gender = Convert.ToChar(reader["Gender"].ToString().Trim());
+                            model.BasicPay = Convert.ToInt32(reader["basic_pay"]);
+                            model.Deductions = Convert.ToDouble(reader["deduction"]);
+                            model.TaxablePay = Convert.ToDouble(reader["taxable_pay"]);
+                            model.IncomeTax = Convert.ToDouble(reader["incomeTax"]);
+                            model.NetPay = Convert.ToDouble(reader["net_pay"]);
+                            model.StartDate = Convert.ToDateTime(reader["start_date"]);
+
+                            Console.Write("{0}\t{1}\t\t{2}\t", model.EmployeeID, model.EmployeeName, model.Gender);
+                            Console.Write("{0}\t\t{1}\t{2}\t{3}\n", model.Department, model.PhoneNumber, model.Address, model.StartDate.ToString("dd-mm-yyyy"));
+                        }
+                    }
+                    else
+                        Console.WriteLine("No data found");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
     }
 }
--- EmployeePayRoll_ADO.NET/EmployeePayRoll_ADO.NET/Program.cs
+++ EmployeePayRoll_ADO.NET/EmployeePayRoll_ADO.NET/Program.cs
@@ -8,31 +8,75 @@ namespace EmployeePayRoll_ADO.NET
         {
             Console.WriteLine("Welcome to Employee PayRoll Service.");
             EmployeeRepo employeeRepo = new EmployeeRepo();
-            employeeRepo.DBconnection();
+            
+            while (true)
+            {
+                Console.Write("1. Check Database Connection \n2. Retrieve Employee Details \n3. Add Employee Details \n4. Update salary of given employee \n5. Retrieve employees in given start date range\n6. Exit");
 
+                Console.Write("\nEnter your choice :");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        employeeRepo.DBconnection();
+                        Console.Write("________________________________________________________________________________________________________________________");
+                        break;
+                    case 2:
+                        employeeRepo.GetAllEmployees();
+                        Console.Write("________________________________________________________________________________________________________________________");
+                        break;
+                    case 3:
+                        EmployeeModel model = new EmployeeModel();
+                        Console.WriteLine("Enter name :");
+                        model.EmployeeName = Console.ReadLine();
+                        Console.WriteLine("Enter Basic pay :");
+                        model.BasicPay = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Start Date :");
+                        model.StartDate = Convert.ToDateTime(Console.ReadLine());
+                        Console.WriteLine("Enter Gender :");
+                        model.Gender = Convert.ToChar(Console.ReadLine());
+                        Console.WriteLine("Enter Phone Number :");
+                        model.PhoneNumber = Console.ReadLine();
+                        Console.WriteLine("Enter Department :");
+                        model.Department = Console.ReadLine();
 
-            EmployeeModel model = new EmployeeModel();
-            //model.EmployeeName = "Twinkle";
-            //model.BasicPay = 75000;
-            //model.StartDate = Convert.ToDateTime("2020-11-03");
-            //model.Gender = 'F';
-            //model.PhoneNumber = "7852149630";
-            //model.Department = "IT";
-            //model.Address = "Lucknow";
-            //model.Deductions = 4540;
-            //model.TaxablePay = 3204;
-            //model.IncomeTax = 4500;
-            //model.NetPay = 52000;
-            //Console.WriteLine(employeeRepo.AddEmployee(model) ? "Record inserted successfully " : "Failed");
-            //employeeRepo.GetAllEmployees();
+                        Console.WriteLine("Enter Address :");
+                        model.Address = Console.ReadLine();
+                        Console.WriteLine("Enter Deductions :");
+                        model.Deductions = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter Taxable Pay :");
+                        model.TaxablePay = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter Income Tax :");
+                        model.IncomeTax = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter Net Pay :");
+                        model.NetPay = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(employeeRepo.AddEmployee(model) ? "Record inserted successfully " : "Failed");
+                        Console.Write("________________________________________________________________________________________________________________________");
+                        break;
+                    case 4:
+                        EmployeeModel models = new EmployeeModel();
+                        Console.Write("Enter name :");
+                        models.EmployeeName = Console.ReadLine();
+                        Console.Write("Enter updated Basic pay :");
+                        models.BasicPay = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter Employee ID :");
+                        models.EmployeeID = Convert.ToInt32(Console.ReadLine());
+                        employeeRepo.UpdateSalary(models);
+                        Console.Write("________________________________________________________________________________________________________________________");
+                        break;
+                    case 5:
+                        employeeRepo.GetAllEmployeesFromDate();
+                        Console.Write("________________________________________________________________________________________________________________________");
+                        break;
+                    case 6:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice!");
+                        break;
 
-            //updation the basic pay with given name and employee id
-            model.EmployeeID = 1;
-            model.BasicPay = 5000000;
-            model.EmployeeName = "natalie";
-            Console.WriteLine(employeeRepo.UpdateSalary(model) ? "updated successfully" : "failed to update");
-
-
+                }
+            }
         }
     }
 }
